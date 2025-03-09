c5b1d6de4507197a0a3d7046f00b75a493f71273
1HyperZ <77121655+1HyperZ@users.noreply.github.com>
2025-03-09 23:22:35 +0200

    changed exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,13 @@ module com.abalone {
     opens com.abalone.controller to javafx.fxml;
     opens com.abalone.model to javafx.fxml;
     opens com.abalone.view to javafx.fxml;
+    opens com.abalone.model.utils to javafx.fxml;
+    opens com.abalone.model.utils.Players to javafx.fxml;
+
 
     exports com.abalone.controller;
     exports com.abalone.model;
     exports com.abalone.view;
+    exports com.abalone.model.utils;
+    exports com.abalone.model.utils.Players;
 }
